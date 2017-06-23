7bcedb330575c7cef85be6fb054aa888062a3cce
monsterreh2b <brant.han@gmail.com>
2017-06-23 13:08:18 -0700

    updated connection and orm

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,4 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-
-// var app = express();
-// var port = 3000;
-
-// // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static("public"));
-
-// // Parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// var exphbs = require("express-handlebars");
-
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
+// Set up MySQL connection.
 
 var mysql = require("mysql");
 

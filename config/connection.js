7d4764fc69cc8a3c7a16b8e4074eecfc24d824e3
monsterreh2b <brant.han@gmail.com>
2017-06-25 15:32:50 -0700

    added JAWSDB URL

--- config/connection.js
+++ config/connection.js
@@ -2,22 +2,37 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 8889,
+//     user: "root",
+//     password: "root",
+//     database: "burgers_db"
+// });
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
 
-    console.log("connected as id " + connection.threadId);
+//     console.log("connected as id " + connection.threadId);
 
-});
+// });
 
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 8889,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+};
+
+connection.connect();
 module.exports = connection;

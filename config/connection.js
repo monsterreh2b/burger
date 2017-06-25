01fa4f4ece5d821bf9cfb77d73c4805ae41e4b0e
monsterreh2b <brant.han@gmail.com>
2017-06-25 15:44:32 -0700

    no port

--- config/connection.js
+++ config/connection.js
@@ -27,7 +27,7 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         host: "localhost",
-        port: 8889,
+        // port: 8889,
         user: "root",
         password: "root",
         database: "burgers_db"
